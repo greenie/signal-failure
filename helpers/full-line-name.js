827a064d70555b1a68696464193cdfefbf9cd2ae
Joe Green <joe@joegreen.co.uk>
2017-10-11 22:06:17 +0100

    Remove hard-coded line names, relying on canonical values from the Interaction Model.

--- helpers/full-line-name.js
+++ helpers/full-line-name.js
@@ -1,31 +1,8 @@
-const lineIdToName = id => {
-  switch (id) {
-    case 'tfl-rail':
-      return 'TfL Rail';
-
-    case 'waterloo-city':
-      return 'Waterloo and City';
-
-    case 'london-overground':
-      return 'Overground';
-
-    case 'hammersmith-city':
-      return 'Hammersmith and City';
-
-    case 'dlr':
-      return 'DLR';
-
-    default:
-      return `${id.charAt(0).toUpperCase()}${id.slice(1)}`;
-  }
-};
-
 export default function fullLineName({ name, id }) {
   const noPrefix = ['tfl-rail'];
-  const noSuffix = noPrefix.concat(['london-overground', 'dlr']);
+  const noSuffix = ['london-overground', 'dlr'].concat(noPrefix);
   const prefix = !noPrefix.includes(id) ? 'the' : null;
   const suffix = !noSuffix.includes(id) ? 'line' : null;
-  const lineName = name ? name : lineIdToName(id);
 
-  return [prefix, lineName, suffix].filter(p => p).join(' ');
+  return [prefix, name, suffix].filter(p => p).join(' ');
 }
