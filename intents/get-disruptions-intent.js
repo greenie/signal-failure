31ff569b8ef3bd89a504e8803aec9a725b8128da
Joe Green <joe@joegreen.co.uk>
2017-10-31 21:01:28 +0000

    Replace axios promise handlers with try/catch.

--- intents/get-disruptions-intent.js
+++ intents/get-disruptions-intent.js
@@ -5,26 +5,13 @@ import fullLineName from '../helpers/full-line-name'
 import getSecret from '../helpers/secrets'
 import log from '../helpers/log'
 
-export default async function () {
-  const { event: { request } } = this
-  const { dialogState, intent: { slots } } = request
+axios.interceptors.response.use(
+  ({ status, data, headers }) => ({ status, data, headers })
+)
 
-  log(request)
-
-  if (dialogState && dialogState !== 'COMPLETED') {
-    log('Line name missing. Asking user to repeat themselves.')
-    return this.emit(':delegate')
-  }
-
+async function getDisruptions(line) {
   const TFL_APP_ID = await getSecret('TFL_APP_ID')
   const TFL_API_KEY = await getSecret('TFL_API_KEY')
-  const line = getCustomSlotValue(slots.Line)
-
-  if (!line.id) {
-    log('Line name missing. Asking user to repeat themselves.')
-    return this.emit(':elicitSlot', 'Line', this.t('UNRECOGNISED_LINE_MESSAGE'))
-  }
-
   const lineDisruptionsUrl = `/Line/${line.id}/Disruption`
   const modeDisruptionsUrl = `/Line/Mode/${line.id}/Disruption`
   const requestOptions = {
@@ -38,55 +25,71 @@ export default async function () {
     timeout: 3000
   }
 
-  axios.interceptors.response.use(
-    ({ status, data, headers }) => ({ status, data, headers })
-  )
+  return await axios(requestOptions)
+}
 
-  axios(requestOptions)
-    .then(response => {
-      const { data } = response
+export default async function () {
+  const { event: { request } } = this
+  const { dialogState, intent: { slots } } = request
 
-      log(response)
+  log(request)
 
-      if (data.length === 0) {
-        const goodService = (line.id === 'tube')
-          ? this.t('GOOD_SERVICE_ALL_LINES_MESSAGE')
-          : responseToSpeak(this.t('GOOD_SERVICE_MESSAGE', fullLineName(line)))
+  if (dialogState && dialogState !== 'COMPLETED') {
+    log('Line name missing. Asking user to repeat themselves.')
+    return this.emit(':delegate')
+  }
 
-        this.emit(
-          ':tellWithCard',
-          goodService,
-          this.t('GOOD_SERVICE_TITLE'),
-          goodService
-        )
-      } else {
-        const description = (line.id === 'tube')
-          ? data.map(({ description }) => description).filter(d => d).join('<break strength="strong" />')
-          : data[0].description
+  const line = getCustomSlotValue(slots.Line)
 
-        this.emit(
-          ':tellWithCard',
-          responseToSpeak(description),
-          this.t('DELAYS_TITLE'),
-          description.replace(/<break[\s+\w*="\w*"]*\s*\/>/g, '\r\n')
-        )
+  if (!line.id) {
+    log('Line name missing. Asking user to repeat themselves.')
+    return this.emit(':elicitSlot', 'Line', this.t('UNRECOGNISED_LINE_MESSAGE'))
+  }
+
+  try {
+    const response = await getDisruptions.call(this, line)
+    const { data } = response
+
+    log(response)
+
+    if (data.length === 0) {
+      const goodService = (line.id === 'tube')
+        ? this.t('GOOD_SERVICE_ALL_LINES_MESSAGE')
+        : this.t('GOOD_SERVICE_MESSAGE', fullLineName(line))
+
+      this.emit(
+        ':tellWithCard',
+        responseToSpeak(goodService),
+        this.t('GOOD_SERVICE_TITLE'),
+        goodService
+      )
+    } else {
+      const description = (line.id === 'tube')
+        ? data.map(({ description }) => description).filter(d => d).join('<break strength="strong" />')
+        : data[0].description
+
+      this.emit(
+        ':tellWithCard',
+        responseToSpeak(description),
+        this.t('DELAYS_TITLE'),
+        description.replace(/<break[\s+\w*="\w*"]*\s*\/>/g, '\n\n')
+      )
+    }
+  } catch (error) {
+    const { response } = error
+
+    if (response) {
+      const { status, data, headers } = response
+
+      log({ status, data, headers })
+
+      if (status === 404) {
+        return this.emit(':ask', this.t('UNRECOGNISED_LINE_MESSAGE'))
       }
-    })
-    .catch(error => {
-      const { response } = error
+    } else {
+      log(error)
+    }
 
-      if (response) {
-        const { status, data, headers } = response
-
-        log({ status, data, headers })
-
-        if (status === 404) {
-          return this.emit(':ask', this.t('UNRECOGNISED_LINE_MESSAGE'))
-        }
-      } else {
-        log(error)
-      }
-
-      this.emit(':tell', this.t('REQUEST_ERROR_MESSAGE'))
-    })
+    this.emit(':tell', this.t('REQUEST_ERROR_MESSAGE'))
+  }
 }
