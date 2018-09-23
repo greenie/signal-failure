3481c7caa1cef17adec3615081cfb1f38923327b
Joe Green <greenie@users.noreply.github.com>
2018-09-23 14:58:59 +0100

    Removes braces from responseToSpeak.

--- helpers/response-to-speak.js
+++ helpers/response-to-speak.js
@@ -1,7 +1,5 @@
-const responseToSpeak = response => {
-  return response
-    .replace(/&/g, 'and')
-    .replace(/DLR/g, '<say-as interpret-as="spell-out">DLR</say-as>')
-}
+const responseToSpeak = response => response
+  .replace(/&/g, 'and')
+  .replace(/DLR/g, '<say-as interpret-as="spell-out">DLR</say-as>')
 
 export default responseToSpeak
