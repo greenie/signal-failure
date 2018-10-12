4b545ba80daf45573398ee1fae346c19287acd9d
Joe Green <greenie@users.noreply.github.com>
2018-10-12 13:15:52 +0100

    Adds new intent handlers for requests that check for up to three lines for disruptions.

---
+++ lambda/custom/intents/get-multi-line-disruptions-intent.js
@@ -0,0 +1,125 @@
+import {
+  allPass,
+  complement,
+  compose,
+  cond,
+  isEmpty,
+  join,
+  map,
+  prop,
+  T
+} from 'ramda'
+import fullLineName from '../helpers/full-line-name'
+import getAllSlotsValues from '../helpers/get-all-slots-values'
+import getIntent from '../helpers/get-intent'
+import getRequest from '../helpers/get-request'
+import intentNameIs from '../helpers/intent-name-is'
+import isIntentRequest from '../helpers/is-intent-request'
+import log from '../helpers/log'
+import requestDialogIs from '../helpers/request-dialog-is'
+import toSentence from '../helpers/to-sentence'
+import getLineDisruptions from '../tfl-api/get-line-disruptions'
+
+const InProgressGetMultiLineDisruptionsIntent = {
+  canHandle (handlerInput) {
+    return compose(
+      allPass([
+        isIntentRequest,
+        intentNameIs('GetMultiLineDisruptionsIntent'),
+        complement(requestDialogIs('COMPLETED'))
+      ]),
+      getRequest
+    )(handlerInput)
+  },
+  handle (handlerInput) {
+    log('In InProgressGetMultiLineDisruptionsIntent')
+
+    const intent = compose(
+      getIntent,
+      getRequest
+    )(handlerInput)
+
+    return handlerInput.responseBuilder
+      .addDelegateDirective(intent)
+      .getResponse()
+  }
+}
+
+const GetMultiLineDisruptionsIntent = {
+  canHandle (handlerInput) {
+    return compose(
+      allPass([
+        [
+          isIntentRequest,
+          intentNameIs('GetMultiLineDisruptionsIntent'),
+          requestDialogIs('COMPLETED')
+        ]
+      ]),
+      getRequest
+    )(handlerInput)
+  },
+  async handle (handlerInput) {
+    log('In GetMultiLineDisruptionsIntent')
+
+    const { t } = handlerInput.attributesManager.getRequestAttributes()
+    const slots = ['LineA', 'LineB', 'LineC']
+    const lines = compose(
+      getAllSlotsValues(slots),
+      prop('slots'),
+      getIntent,
+      getRequest
+    )(handlerInput)
+
+    let disruptions
+
+    try {
+      disruptions = await getLineDisruptions(...map(prop('id'), lines))
+    } catch (error) {
+      log(error.message)
+
+      return handlerInput.responseBuilder
+        .speak(t('REQUEST_ERROR_MESSAGE'))
+        .getResponse()
+    }
+
+    const [title, description] = cond([
+      [
+        isEmpty,
+        () => {
+          const message = compose(
+            toSentence,
+            map(fullLineName)
+          )(lines)
+
+          return [
+            t('GOOD_SERVICE_TITLE'),
+            t('GOOD_SERVICE_MESSAGE', message)
+          ]
+        }
+      ],
+      [
+        T,
+        disruptions => [
+          t('DELAYS_TITLE'),
+          compose(
+            join('\n\n'),
+            map(prop('description'))
+          )(disruptions)
+        ]
+      ]
+    ])(disruptions)
+
+    return handlerInput.responseBuilder
+      .speak(description)
+      .withSimpleCard(
+        title,
+        description
+      )
+      .getResponse()
+  }
+}
+
+export {
+  InProgressGetMultiLineDisruptionsIntent,
+  GetMultiLineDisruptionsIntent
+}
