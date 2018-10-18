aa1ae8de49c23f9dd37a30ba75c3cb8e77cf8552
Joe Green <greenie@users.noreply.github.com>
2018-10-18 23:10:26 +0100

    Converts responseToSpeak to use Ramda.

--- lambda/custom/helpers/response-to-speak.js
+++ lambda/custom/helpers/response-to-speak.js
@@ -1,5 +1,8 @@
-const responseToSpeak = response => response
-  .replace(/&/g, 'and')
-  .replace(/DLR/g, '<say-as interpret-as="spell-out">DLR</say-as>')
+import { compose, replace } from 'ramda'
+
+const responseToSpeak = compose(
+  replace(/&/g, 'and'),
+  replace(/DLR/g, '<say-as interpret-as="spell-out">DLR</say-as>')
+)
 
 export default responseToSpeak
