d2203db68688f74afbdef07b7a47f5729b242036
Joe Green <greenie@users.noreply.github.com>
2018-12-29 20:52:49 +0000

    Updates i18next usage to work with their new Promise-based API.

--- lambda/custom/interceptors/localisation-interceptor.js
+++ lambda/custom/interceptors/localisation-interceptor.js
@@ -1,19 +1,22 @@
-import i18n from 'i18next'
+import i18next from 'i18next'
 import sprintf from 'i18next-sprintf-postprocessor'
 import translations from '../resources/translations'
 
 const LocalisationInterceptor = {
-  process (handlerInput) {
-    const client = i18n.use(sprintf).init({
-      lng: handlerInput.requestEnvelope.request.locale,
-      overloadTranslationOptionHandler: sprintf.overloadTranslationOptionHandler,
-      resources: translations,
-      returnObjects: true
-    })
+  async process (handlerInput) {
+    const { attributesManager, requestEnvelope } = handlerInput
 
-    handlerInput.attributesManager.setRequestAttributes({
-      t: (...args) => client.t(...args)
-    })
+    await i18next
+      .use(sprintf)
+      .init({
+        lng: requestEnvelope.request.locale,
+        overloadTranslationOptionHandler: sprintf.overloadTranslationOptionHandler,
+        resources: translations,
+        returnObjects: true
+      })
+      .then(t => attributesManager.setRequestAttributes({
+        t: (...args) => t(...args)
+      }))
   }
 }
 
